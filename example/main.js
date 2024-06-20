714150db8e639bf9d0f73826dbfe41202baaec68
andron13 <andron13@gmail.com>
2024-06-20 18:14:17 +0200

    feat: class-01

---
+++ example/main.js
@@ -0,0 +1,12 @@
+function calculate(num1, num2) {
+  return num1 + num2;
+}
+
+document.getElementById("button").addEventListener("click", () => {
+  const num1 = parseFloat(document.getElementById('num1').value);
+  const num2 = parseFloat(document.getElementById('num2').value);
+
+  const result = calculate(num1, num2);
+
+  document.getElementById('result').innerHTML = 'Result: ' + result;
+});
