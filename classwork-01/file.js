30a50e6541fc1adf057753586b22a4c9cc562019
andron13 <andron13@gmail.com>
2024-06-24 14:23:02 +0200

    feat: test commit

---
+++ classwork-01/file.js
@@ -0,0 +1,29 @@
+function addition(num1, num2) {
+    return num1 + num2;
+}
+
+// const myFirstResult = addition(4,5);
+
+function toPrint() {
+    const myFirstResult = addition(4, 5);
+    console.log("myFirstResult: ", myFirstResult);
+}
+
+toPrint();
+
+// CamelCase
+// так делать нельзя
+const primerPeremennoj = 0;
+const exampleVariableInJavaScript = 0;
+
+let test1;
+let test2 = 0;
+test1 = 0;
+
+// var, let, const
+// let, const
+
+const constanta = 0;
+const MATH_PI = 3.14;
+// constanta = 10;
+test1 = 100;
