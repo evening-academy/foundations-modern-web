dfdb13f6d2afa74a34cfdf891ca61f3e0de36afa
andron13 <andron13@gmail.com>
2024-06-26 14:22:28 +0200

    feat: hw solved

--- hw-01-solution.js
+++ hw-01-solution.js
@@ -1,3 +1,24 @@
 // Попытка решения домашнего задания
 
 // А так?
+
+function findSquarePerimeter(site) {
+    return site * 4;
+}
+
+function getRandom( max) {
+    return Math.floor(Math.random() * max);
+    const bigIntValue = (2n ** 64n) - 1n;
+    console.log(bigIntValue);
+}
+
+console.log(getRandom(100))
+//
+// // countChessboardGrains
+//
+// 1 + 2 + 4 + 8 ...
+//
+// 2^0 + 2^1  + 2^2  + 2^3   ...  + 2^63
+
+2^64  - 1
+
