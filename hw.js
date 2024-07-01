a7368ea5f9628ffd3ccad7434625f78607eaa16c
Iryna Yelunina <lucky.asterisk@gmail.com>
2024-07-01 10:18:29 +0200

    hw/sol-03

---
+++ hw.js
@@ -0,0 +1,50 @@
+function helloName(name) {
+    return "Hello " + name + "!";
+}
+
+let name = "Bob";
+console.log(helloName(name));
+
+function makeOutWord(template, word) {
+    // Извлекаем первую часть строки
+    let firstPart = template.substring(0, 2);
+    // Извлекаем вторую часть строки
+    let secondPart = template.substring(2);
+    // Вставляем слово между первой и второй частями
+    return firstPart + word + secondPart;
+}
+
+let template = "<<>>";
+let word = "hello";
+console.log(makeOutWord(template, word));
+
+function firstHalf(str) {
+    // Находим половину длины строки
+    let halfLength = str.length / 2;
+    // Извлекаем первую половину строки
+    return str.substring(0, halfLength);
+}
+
+let inputStr = "DcjhFgdluGhjku";
+console.log(firstHalf(inputStr));
+
+function nonStart(str1, str2) {
+    // Извлекаем подстроки без первого символа
+    let newStr1 = str1.substring(1);
+    let newStr2 = str2.substring(1);
+    // Конкатенируем подстроки
+    return newStr1 + newStr2;
+}
+
+let str1 = "This";
+let str2 = "Is";
+console.log(nonStart(str1, str2));
+
+function theEnd(str, front) {
+    // Если front равен true, вернуть первый символ строки
+    // Иначе вернуть последний символ строки
+    return front ? str.charAt(0) : str.charAt(str.length - 1);
+}
+
+console.log(theEnd("nonStart", true));
+console.log(theEnd("nonStart", false));
